10d32d3907f0268a82b870ca20155bed7959ecfe
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-06-15 15:04:14 +0300

    .

--- server.js
+++ server.js
@@ -7,36 +7,21 @@ const sendmonyRouter = require("./routes/send_mony.js");
 const counterRouter = require("./routes/counter.js");
 const notifications = require("./routes/notifications.js");
 const agentsRouter = require("./routes/agents.js");
+
 require("./cron");
 
-const socketIo = require("socket.io");
-const timerRoute = require("./routes/timerRoute");
+
 const app = express();
-
 app.use(express.json());
-app.use("/uploads", express.static("./uploads"));
+app.use("/uploads", express.static("./" + "uploads"));
 
+sequelize.sync({
+  //  alter: true 
+    force: false,
+ })
+    .then(() => console.log("✅ Database & User table synced!"))
+    .catch(err => console.error("❌ Error syncing database:", err));
 
-const server = require("http").createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "*",
-  },
-});
-app.use("/timer", timerRoute(io));
-
-io.on("connection", (socket) => {
-  console.log("عميل جديد متصل عبر Socket.io");
-
-  socket.on("disconnect", () => {
-    console.log("تم قطع الاتصال");
-  });
-});
-
-sequelize
-  .sync({ force: false })
-  .then(() => console.log("✅ Database & User table synced!"))
-  .catch((err) => console.error("❌ Error syncing database:", err));
 
 app.use("/", usersRouter);
 app.use("/", sendmonyRouter);
@@ -45,6 +30,7 @@ app.use("/", counterRouter);
 app.use("/", notifications);
 app.use("/", agentsRouter);
 
-server.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
+
+app.listen( 3000 , () => {
+    console.log(`🚀 Server running on http://localhost:3000`);
 });
