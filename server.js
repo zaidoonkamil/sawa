d934ba6e2ce520cbbb2ba776c9a9f0f708908ffe
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-07-13 19:53:56 +0300

    /

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/uploads", express.static("./" + "uploads"));
 
 sequelize.sync({
-    alter: true
+   // alter: true
    // force: false,
  }).then(() => console.log("✅ Database & User table synced!"))
   .catch(err => console.error("❌ Error syncing database:", err));
